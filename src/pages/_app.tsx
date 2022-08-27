bf63c3d83e845e4718a5ed6bd9827ef430b83849
AlexJamisone <business.homeit@gmail.com>
2022-08-27 15:26:47 +0300

    add tips page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Background from '../components/Background/Background'
 import Navigation from '../components/Navigation/Navigation'
-import styles from '../styles/index.module.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
---
+++ src/pages/tips.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/future/image'
+import { motion } from 'framer-motion'
+import styles from '../styles/tips.module.scss'
+
+
+
+const TipsCard = () => {
+	const barista = [
+		{
+			name: 'Саша',
+			link: process.env.SASH_TINKOFF,
+			avatar: '/static/avatar/sasha.png',
+		},
+		{
+			name: 'Оля',
+			link: process.env.OLI_TINKOFF,
+			avatar: '/static/avatar/oli.png',
+		},
+		{
+			name: 'Лёша',
+			link: process.env.ALEX_TINKOFF,
+			avatar: '/static/avatar/alex.png',
+		},
+	]
+	return (
+		<div className={styles.container}>
+			{barista.map((person, index) => {
+				const { name, link, avatar } = person
+				return (
+					<motion.a
+						initial={{ y: 100, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ type: 'spring' }}
+						href={link}
+						target="_blank"
+						rel="noreferrer"
+						key={index}
+						className={styles.link}
+					>
+						<Image
+                            className={styles.avatar}
+                            width={250}
+                            height={260}
+							src={avatar}
+							alt={`${name}`}
+						/>
+						<h1 className={styles.name}>{name}</h1>
+					</motion.a>
+				)
+			})}
+		</div>
+	)
+}
+
+export default TipsCard
